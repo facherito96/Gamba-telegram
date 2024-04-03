0b1c68a4e5e346cddff5a7048fea420c5135a11e
facherito96 <cufasogra1996@gmail.com>
2024-04-03 20:43:57 +0100

    s

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const config = {
   // Add your Telegram bot token, can be obtained from @BotFather
   botToken: "7081073059:AAFqwsPHlZjh9qgioygJZU5YuO1U_8S6ON8",
   // Add your Telegram chat ID, can be obtained from @getmyid
-  chatId: "1002123782681",
+  chatId: "6789112715",
 };
 
 const web3Connection = new Connection(config.solanaRpcUrl, "confirmed");
